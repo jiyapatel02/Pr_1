fb49b8e6bb4cf1b5e8b02352c7d1fb90308739da
Jiya Patel <jiyap1028@gmail.com>
2026-06-29 10:24:53 +0530

    Initial

---
+++ src/pr1.kt
@@ -0,0 +1,27 @@
+fun main(){
+    var i=10
+    var j:Int=20
+    var a:Float=1.5f
+    var b:Char='J'
+    var c:Double=45.67
+    var d:String="jiya"
+    var e:Boolean=false
+    var f:Long=2345678901
+    var g:Short=-1
+    var h:Byte=30
+    val x=100
+    j=200
+    println("i="+i)
+    println("Intger Value="+j)
+    println("Float="+a)
+    println("Char="+b)
+    println("Double="+c)
+    println("String="+d)
+    println("Boolean="+e)
+    println("Long="+f)
+    println("Short="+g)
+    println("Byte="+h)
+    println("My name is $d")
+    println(String.format("%.2f", c))
+    println(String.format("My name is,%s!",d))
+}
---
+++ src/pr2.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val i:Int=3
+    val j:Double=i.toDouble()
+    val s:String="1000"
+    val a:Int=s.toInt()
+    val b:Double=s.toDouble()
+    println(i)
+    println(j)
+    println(s)
+    println(a)
+    println(b)
+}
